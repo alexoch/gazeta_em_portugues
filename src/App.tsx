a4071fa95c079296b3d326622bf7f1ff326a2222
Ocheretianyi Oleksandr <s.ocheretyany@gmail.com>
2025-11-17 22:21:50 +0000

    fix production

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Header from './components/Header/Header'
 import './styles/globals.scss'
 import './App.css'
 import WordOfTheDay from './components/WordOfTheDay/WordOfTheDay'
-import EventsOfTheDay from './components/EventsOfTheDay/EventsOfTheDay'
 
 function App() {
   
@@ -10,7 +9,6 @@ function App() {
     <>
       <Header/>
       <WordOfTheDay/>
-      <EventsOfTheDay/>
     </>
   )
 }
